481d4ba4c4b6909c4d0947becc5027fcfc421e63
Alexandru Grigorescu <alex.grigorescu46@gmail.com>
2023-02-08 18:33:22 +0200

    Adjust firebase config

--- src/configs/firebase.js
+++ src/configs/firebase.js
@@ -1,11 +1,10 @@
 const firebaseConfig = {
-    apiKey: "AIzaSyCcbZMkwd4ZV_1DiH0R3TZFCg9GKaIMeoM",
-    authDomain: "react-sirluggia-shop.firebaseapp.com",
-    databaseURL: "https://react-sirluggia-shop.firebaseio.com",
-    projectId: "react-sirluggia-shop",
-    storageBucket: "react-sirluggia-shop.appspot.com",
-    messagingSenderId: "184253035461",
-    appId: "1:184253035461:web:20fe5c173c7675c74c51ac"
+    apiKey: "AIzaSyAiMBFem-saINk445h896uDPJHk8LjFnZ0",
+    authDomain: "react-sirluggia-shop-98a55.firebaseapp.com",
+    projectId: "react-sirluggia-shop-98a55",
+    storageBucket: "react-sirluggia-shop-98a55.appspot.com",
+    messagingSenderId: "627854035452",
+    appId: "1:627854035452:web:c8254b33a53d93f8ea8a79"
 };
 
 export default firebaseConfig;
