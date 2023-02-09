e3b4cb8b72087533bbefca1a46efc93c8330db30
Alexandru Grigorescu <alex.grigorescu46@gmail.com>
2023-02-09 19:05:25 +0200

    Add message when there are no favorite products

--- src/pages/Favorites.jsx
+++ src/pages/Favorites.jsx
@@ -2,19 +2,27 @@ import React from 'react';
 import Layout from '../components/Layout';
 import { connect } from 'react-redux';
 import './Favorites.css';
+import { Link } from 'react-router-dom';
 import ProductItem from '../components/ProductItem';
 
 function Favorites(props) {
     return (
-        <Layout>
-            <div className="row my-4">
-                { props.favorites.map((product) => {
-                    return <ProductItem
-                        {...product}
-                        key={product.id}
-                    />
-                })}
-            </div>
+        <Layout> 
+            {
+                props.favorites.length 
+                ? <div className="row my-4">
+                      { props.favorites.map((product) => {
+                            return <ProductItem
+                                {...product}
+                                key={product.id}
+                            />
+                    })}
+                  </div>
+                : <div className="d-flex flex-column align-items-center">
+                      <p className="h3">Nu ai produse favorite!</p>
+                      <Link to="/"><button className="btn btn-outline-dark">Inapoi la home</button></Link>
+                  </div>
+            }
         </Layout>
     );
 }
